6d360639319c7dc83ea059e3264ef29ba0f70efd
karthika <you@example.com>
2020-09-15 07:10:45 +0530

    login bootstrap & validation added

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,8 +5,10 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.precompile += %w( user.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( heart.js )
+Rails.application.config.assets.precompile += %w( validate.js )
